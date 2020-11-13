d49254fd4385b2676c0003e4ed8ce566ec214af3
AndyMMunro <agovitadventures@gmail.com>
2020-11-13 14:21:53 -0800

    changed file structure for heroku deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("Develop/public"));
 
 mongoose.connect("mongodb://localhost/ledger", {
   useNewUrlParser: true,
@@ -21,7 +21,7 @@ mongoose.connect("mongodb://localhost/ledger", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./Develop/routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
