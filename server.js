73bbdd38a035ebcdbea5dc11e970235099908dff
AndyMMunro <agovitadventures@gmail.com>
2020-11-13 14:37:02 -0800

    changed file structure for heroku deployment

--- server.js
+++ server.js
@@ -15,11 +15,19 @@ app.use(express.json());
 
 app.use(express.static("Develop/public"));
 
-mongoose.connect("mongodb://localhost/ledger", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+// mongoose.connect("mongodb://localhost/Ledger", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // routes
 app.use(require("./Develop/routes/api.js"));
 
