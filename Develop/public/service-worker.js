606bb803973e50a336d6b62f3de4a583591b96fb
AndyMMunro <agovitadventures@gmail.com>
2020-11-17 18:18:27 -0800

    added working cache no 404 errors does not cache the previous data from the database.

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,15 +1,11 @@
 const FILES_TO_CACHE = [
   "/",
+  // "/models/transactions",
   "/index.html",
-  "/assets/css/style.css",
-  "/assets/js/loadPosts.js",
-  "/assets/images/Angular-icon.png",
-  "/assets/images/React-icon.png",
-  "/assets/images/Vue.js-icon.png",
+  "/styles.css",
   "/manifest.webmanifest",
-  "/favicon.ico",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-512x512.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
